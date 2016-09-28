10741874c83611fbbdaea9fd622f20c5e6dd4333
Ben Scheiner <ben@carevoyance.com>
2016-09-27 22:58:46 -0700

    p5 boilerplate

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {        
+    createCanvas(window.innerWidth, window.innerHeight);  
+    stroke(255); 
+    strokeWeight(1);
+    frameRate(50);
+    background([0,0,0]);
+    fill(255);  
+}
+
+function mousePressed() {
+}
+
+function draw() {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
